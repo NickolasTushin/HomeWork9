082dd181c8b1b636943a997515808d4738706e19
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2022-12-29 17:45:19 +0300

    ADD Tasks

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int rec(int a, int b)
+{
+    if (a == b)
+        return b;
+    return rec(a + 1, b) + a;
+}
+Console.WriteLine($" Сумма натуральных элементов от M до N = {rec(m, n)}");
---
+++ Task68/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(m < 0)
+{
+Console.WriteLine("Введите неотрицательное число!!!");
+Console.WriteLine("Введите неотрицательное число m: ");
+m = Convert.ToInt32(Console.ReadLine());
+}
+while(n < 0)
+{
+Console.WriteLine("Введите неотрицательное число!!!");
+Console.WriteLine("Введите неотрицательное число n: ");
+n = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+
+int rec(int a, int b)
+ {
+     if (a == 0)
+         return b + 1;
+     if (b == 0)
+         return rec(a - 1, 1);
+     else 
+         return rec(a - 1, rec(a, b - 1));
+ }
+
+ Console.Clear();
+
+ Console.WriteLine($"Результат вычислений = {rec (m, n)}");
